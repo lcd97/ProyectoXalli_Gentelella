18535c7530d6b4d9e88943fda7ae2574164b1bbf
lcd97 <danycordero9@gmail.com>
2020-08-05 20:47:27 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoXalli_Gentelella/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoXalli_Gentelella.Startup))]
+namespace ProyectoXalli_Gentelella
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
