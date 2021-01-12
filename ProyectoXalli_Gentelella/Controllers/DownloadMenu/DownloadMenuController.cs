391daeb33e094f0c3b81634884cdaedaf2835205
lcd97 <danycordero9@gmail.com>
2021-01-12 11:36:14 -0600

    - Agregando menu -

---
+++ ProyectoXalli_Gentelella/Controllers/DownloadMenu/DownloadMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoXalli_Gentelella.Controllers.DownloadMenu {
+    [AllowAnonymous]
+    public class DownloadMenuController : Controller {
+        // GET: DownloadMenu
+        [AllowAnonymous]
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
